4f3c515fb42f6358e2c89b27e5e46f2f2a94c6ef
Md-meraj21 <mdmerajansari975@gmail.com>
2026-05-20 10:54:04 +0530

    for render

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [process.env.FRONTEND_URL || 'http://localhost:3000',
-    // "https://canteen-yuvg.onrender.com"
+    "https://canteen-3-bj2x.onrender.com/"
     ],
   credentials: true
 }));
